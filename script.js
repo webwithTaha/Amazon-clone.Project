0a02298236e73b3b92281d26bad6a4a68a775334
webwithTaha <codewithtaha14@gmail.com>
2024-09-13 15:25:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+    } else {
+        sidebar.style.width = "250px";
+    }
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+}
